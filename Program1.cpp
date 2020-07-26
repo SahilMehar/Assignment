6c49e1d34b225cd0fe8f890dff149b4482d54bc2
SahilMehar <68806020+SahilMehar@users.noreply.github.com>
2020-07-26 14:42:51 +0530

    Add files via upload

---
+++ Program1.cpp
@@ -0,0 +1,23 @@
+//WAP to find largest of three numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout << "Enter three numbers ";
+    cin >> a >> b >> c;
+    if(a>=b && a>=c)
+    {
+        cout << "Largest no. is: "<<a;
+    }
+    if(b>=a && b>=c)
+    {
+        cout << "Largest no. is: "<<b;
+    }
+    if(c>=a && c>=b)
+    {
+        cout << "Largest no. is: "<<c;
+    }
+    return 0;
+}
+
